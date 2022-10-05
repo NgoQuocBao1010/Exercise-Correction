1361f732a9ab45f46bd7a6bc501965f1b09bdc1a
ngoquocbao1010 <totti.chelsea@gmail.com>
2022-10-05 10:26:00 +0700

    👨‍💻 feat: add Vue js for web client

---
+++ web/client/src/App.vue
@@ -0,0 +1,19 @@
+<script setup></script>
+
+<template>
+    <main>
+        <h1>Hello from Quoc Bao</h1>
+    </main>
+</template>
+
+<style lang="scss">
+body {
+    height: 100vh;
+    background-color: black;
+
+    h1 {
+        color: white;
+        text-align: center;
+    }
+}
+</style>
