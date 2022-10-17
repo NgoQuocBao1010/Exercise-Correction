0e424fea201d9e9d40e7888aad84eabea64f9b7c
ngoquocbao1010 <totti.chelsea@gmail.com>
2022-10-17 11:06:45 +0700

    👨‍💻 feat: working on stream data with Django

--- web/server/api/urls.py
+++ web/server/api/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path("", views.api, name="api"),
+    path("/", include("realtime.urls")),
 ]
