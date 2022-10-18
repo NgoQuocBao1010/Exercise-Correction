b975d2c5a39774b3c8f63e904a684a8aef53a277
ngoquocbao1010 <totti.chelsea@gmail.com>
2022-10-18 18:21:46 +0700

    👨‍💻 feat: stream video successfully

--- web/server/exercise_correction/settings.py
+++ web/server/exercise_correction/settings.py
@@ -114,8 +114,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+
+MEDIA_URL = "/media/"
+
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "static/media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- web/server/stream_video/views.py
+++ web/server/stream_video/views.py
@@ -1,8 +1,21 @@
+import os
+from django.http import StreamingHttpResponse
+from wsgiref.util import FileWrapper
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from datetime import datetime
+from django.conf import settings
 
 
 @api_view(["GET"])
 def api(request):
-    return Response("This is from video streaming")
+    path = f"{settings.MEDIA_ROOT}/plank1.mp4"
+    video_size = os.path.getsize(path)
+
+    chunk_size = 8000
+
+    response = StreamingHttpResponse(
+        FileWrapper(open(path, "rb"), chunk_size), content_type="video/mp4"
+    )
+
+    response["Content-Length"] = video_size
+    return response
