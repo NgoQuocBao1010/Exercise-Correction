804666a492bae70bb45ef459e21ffaaa2c177de6
ngoquocbao1010 <totti.chelsea@gmail.com>
2022-11-11 14:31:57 +0700

    👨‍💻 feat: style for video processed website

--- web/client/src/views/VideoStreaming.vue
+++ web/client/src/views/VideoStreaming.vue
@@ -1,69 +1,156 @@
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import axios from "axios";
-import Video from "../components/Video.vue";
 
-const video = ref(null);
-const processing = ref(false);
-const videoName = ref(null);
+import Dropzone from "../components/Dropzone.vue";
+import DropzoneLoading from "../components/DropzoneLoading.vue";
+
+const EXERCISES = ["squat", "plank", "bicep curl", "lunge"];
+
+const submitData = ref({
+    videoFile: null,
+    exerciseType: null,
+});
 const processedData = ref(null);
-
-const onFileUpload = (event) => {
-    const target = event.target;
-
-    if (target && target.files) {
-        video.value = target.files[0];
-    }
-};
+const isProcessing = ref(false);
 
 const uploadToServer = async () => {
-    if (!video.value) {
-        alert("No file selected");
+    if (!submitData.value.videoFile) {
+        alert("No video selected");
+        return;
+    }
+
+    if (!submitData.value.exerciseType) {
+        alert("No exercise type selected");
         return;
     }
 
     processedData.value = null;
     try {
-        processing.value = true;
+        isProcessing.value = true;
         const { data } = await axios.post(
-            "http://127.0.0.1:8000/api/video/upload",
-            { file: video.value },
+            `http://127.0.0.1:8000/api/video/upload?type=${submitData.value.exerciseType}`,
+            { file: submitData.value.videoFile },
             {
                 headers: {
                     "Content-Type": "multipart/form-data",
                 },
             }
         );
-
         processedData.value = data;
     } catch (e) {
         console.log("Error: ", e);
     } finally {
-        processing.value = false;
+        isProcessing.value = false;
     }
 };
 </script>
 
 <template>
-    <template v-if="null">
-        <Video></Video>
-    </template>
+    <!-- Input section -->
+    <section class="input-section">
+        <Dropzone
+            v-show="!isProcessing"
+            @file-uploaded="(file) => (submitData.videoFile = file)"
+        />
+        <DropzoneLoading v-show="isProcessing" />
 
-    <input type="file" @change="onFileUpload" accept="video/*" />
-    <button @click="uploadToServer">Upload!</button>
+        <div class="right-container">
+            <!-- exercises selection -->
+            <div class="exercises-container">
+                <p
+                    class="exercise"
+                    v-for="exercise in EXERCISES"
+                    :class="{ active: submitData.exerciseType == exercise }"
+                    @click="submitData.exerciseType = exercise"
+                >
+                    {{ exercise }}
+                </p>
+            </div>
 
-    <h2 v-if="processing">Processing ...</h2>
+            <button class="process-btn" @click="uploadToServer">
+                <span>Process!</span>
+            </button>
+        </div>
+    </section>
 
-    <!-- <Video v-if="videoName" :videoName="videoName"> </Video> -->
+    <!-- Results section -->
+    <section>
+        <template v-if="processedData">
+            <h3>There are {{ processedData.details.length }} errors found</h3>
 
-    <template v-if="processedData">
-        <h3>There are {{ processedData.details.length }} errors found</h3>
-
-        <template v-for="error in processedData.details">
-            <p>Class: {{ error.stage }}</p>
-            <img :src="`${error.frame}`" alt="" width="600" />
+            <template v-for="error in processedData.details">
+                <p>Class: {{ error.stage }}</p>
+                <img :src="`${error.frame}`" alt="" width="600" />
+            </template>
         </template>
-    </template>
+    </section>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.input-section {
+    display: flex;
+    gap: 1rem;
+
+    * {
+        flex: 1;
+    }
+
+    .right-container {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+
+        .exercises-container {
+            display: flex;
+            flex-wrap: wrap;
+            gap: 1rem;
+            margin-bottom: 1rem;
+
+            .exercise {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                padding: 1rem 0;
+                flex: 45%;
+                color: var(--secondary-color);
+                text-transform: uppercase;
+                border: 3px solid var(--primary-color);
+                border-radius: 0.3rem;
+                cursor: pointer;
+                transition: all 0.25s ease;
+
+                &:hover {
+                    box-shadow: 0 6px 18px 0 rgba(#000, 0.1);
+                    transform: translateY(-6px);
+                }
+
+                &.active {
+                    background-color: var(--primary-color);
+                    font-weight: 700;
+                }
+            }
+        }
+
+        .process-btn {
+            border: none;
+            background-color: var(--primary-color);
+            padding: 1.25rem 0;
+
+            color: whitesmoke;
+            font-size: 1.25rem;
+            font-weight: 700;
+            cursor: pointer;
+            border-radius: 8px;
+            transition: all 0.25s ease;
+
+            &:hover {
+                box-shadow: 0 6px 18px 0 rgba(#000, 0.1);
+                color: var(--primary-color);
+                border-color: transparent;
+                background-color: transparent;
+            }
+        }
+    }
+}
+</style>
