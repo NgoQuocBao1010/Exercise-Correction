7e174f63e3173cf60bb77050fd5dff9cff2ef624
ngoquocbao1010 <totti.chelsea@gmail.com>
2022-11-11 14:29:35 +0700

    👨‍💻 feat: file input dropzone component

---
+++ web/client/src/components/Dropzone.vue
@@ -0,0 +1,132 @@
+<script setup>
+import { ref, watch, computed } from "vue";
+
+// TODO: handle only video file
+const dropzoneInput = ref(null);
+const uploadedVideoFile = ref(null);
+const isUploaded = computed(() => (uploadedVideoFile.value ? true : false));
+const isDragOver = ref(false);
+
+// Emit events
+const emit = defineEmits(["fileUploaded"]);
+watch(
+    () => uploadedVideoFile.value,
+    (newData, oldData) => {
+        if (newData) {
+            emit("fileUploaded", newData);
+        }
+    }
+);
+
+// * Handle Drag Events
+const removeDragEventDefault = (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+};
+const handleDragOver = (event) => {
+    removeDragEventDefault(event);
+    isDragOver.value = true;
+};
+const handleDragLeave = (event) => {
+    removeDragEventDefault(event);
+    isDragOver.value = false;
+};
+const handleDrop = (event) => {
+    removeDragEventDefault(event);
+    isDragOver.value = false;
+
+    let videoFile = event.dataTransfer.files[0];
+
+    let dataTransfer = new DataTransfer();
+    dataTransfer.items.add(videoFile);
+    let filesToBeAdded = dataTransfer.files;
+
+    dropzoneInput.value.files = filesToBeAdded;
+    uploadedVideoFile.value = videoFile;
+};
+
+// Handle File Input
+const openFileInput = () => {
+    dropzoneInput.value.click();
+};
+const handleClickUpload = (event) => {
+    const target = event.target;
+    if (target && target.files) {
+        uploadedVideoFile.value = target.files[0];
+    }
+};
+
+// Other utils func
+const byteToMB = (bytes) => {
+    return Math.round(bytes / 1000000);
+};
+</script>
+
+<template>
+    <div
+        class="dropzone"
+        :class="{ 'dropzone-dragging': isDragOver }"
+        id="dropzone"
+        @drag="removeDragEventDefault"
+        @dragstart="removeDragEventDefault"
+        @dragend="removeDragEventDefault"
+        @dragover="handleDragOver"
+        @dragenter="handleDragLeave"
+        @dragleave="removeDragEventDefault"
+        @drop="handleDrop"
+        @click="openFileInput"
+    >
+        <!-- Initial Stage -->
+        <template v-if="!isUploaded">
+            <i class="fa-solid fa-cloud-arrow-up dropzone-icon"></i>
+            Drop files or Click here to select files to upload.
+        </template>
+
+        <!-- Uploaded State -->
+        <template v-else>
+            <i class="fa-regular fa-file-video dropzone-icon"></i>
+            {{ uploadedVideoFile.name }}
+            ({{ byteToMB(uploadedVideoFile.size) }} MB)
+        </template>
+
+        <input
+            type="file"
+            name="files"
+            class="dropzone-input"
+            ref="dropzoneInput"
+            @change="handleClickUpload"
+        />
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.dropzone {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border: 0.2rem dashed var(--primary-color);
+    padding: 2rem;
+    border-radius: 0.25rem;
+    background-color: #fff;
+    font-size: 1.25rem;
+    transition: 0.25s background-color ease-in-out;
+    cursor: pointer;
+
+    &-dragging,
+    &:hover {
+        background-color: lighten(#41b883, 38%);
+    }
+
+    &-icon {
+        display: block;
+        font-size: 3rem;
+        margin: 0 auto 1.5rem;
+        color: var(--primary-color);
+    }
+
+    &-input {
+        display: none;
+    }
+}
+</style>
---
+++ web/client/src/components/DropzoneLoading.vue
@@ -0,0 +1,76 @@
+<script setup></script>
+
+<template>
+    <div class="dropzone">
+        <div class="loading-animation">
+            <div class="dot"></div>
+            <div class="dot"></div>
+            <div class="dot"></div>
+        </div>
+
+        Processing ...
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.dropzone {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border: 0.2rem dashed var(--primary-color);
+    padding: 2rem;
+    border-radius: 0.25rem;
+    background-color: #fff;
+    font-size: 1.25rem;
+    transition: 0.25s background-color ease-in-out;
+    cursor: pointer;
+
+    &-icon {
+        display: block;
+        font-size: 3rem;
+        margin: 0 auto 1.5rem;
+        color: var(--primary-color);
+    }
+
+    &-input {
+        display: none;
+    }
+}
+
+.loading-animation {
+    display: flex;
+    gap: 1rem;
+    margin-bottom: 2rem;
+
+    .dot {
+        display: inline-block;
+        width: 20px;
+        aspect-ratio: 1 / 1;
+        border-radius: 100%;
+        background-color: var(--primary-color);
+    }
+}
+
+.loading-animation .dot:nth-last-child(1) {
+    animation: loading 0.6s 0.1s linear infinite;
+}
+.loading-animation .dot:nth-last-child(2) {
+    animation: loading 0.6s 0.2s linear infinite;
+}
+.loading-animation .dot:nth-last-child(3) {
+    animation: loading 0.6s 0.3s linear infinite;
+}
+
+@keyframes loading {
+    0% {
+        transform: translate(0, 0);
+    }
+    50% {
+        transform: translate(0, 15px);
+    }
+    100% {
+        transform: translate(0, 0);
+    }
+}
+</style>
