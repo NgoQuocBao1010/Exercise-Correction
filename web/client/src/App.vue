72bf68a9b6cd8269c317606e002f43e0acff9e8d
ngoquocbao1010 <totti.chelsea@gmail.com>
2022-11-16 16:27:38 +0700

    👨‍💻 feat: web footer

--- web/client/src/App.vue
+++ web/client/src/App.vue
@@ -13,12 +13,29 @@
         <!-- Page View -->
         <RouterView />
     </main>
+
+    <!-- Footer -->
+    <footer>
+        <a
+            href="https://twitter.com/ngohquocbao1010"
+            target="_blank"
+            class="twitter"
+        >
+            @NgoQuocBao
+        </a>
+
+        <a
+            href="https://github.com/NgoQuocBao1010/Exercise-Correction"
+            target="_blank"
+            class="github"
+        >
+            <i class="fa-brands fa-github"></i>
+        </a>
+    </footer>
 </template>
 
 <style lang="scss">
 body {
-    // background-color: black;
-
     nav {
         margin: 3rem 0;
         text-align: center;
@@ -36,6 +53,32 @@ body {
     main {
         margin-inline: auto;
         width: 90vw;
+    }
+
+    footer {
+        width: 90vw;
+        border-top: 1px solid var(--primary-color);
+        position: fixed;
+        bottom: 0;
+        left: 50%;
+        transform: translateX(-50%);
+        margin-inline: auto;
+        padding: 0.3rem 0;
+        text-align: center;
+
+        a {
+            font-family: var(--font-family);
+            color: var(--primary-color);
+        }
+
+        .twitter {
+            font-weight: 700;
+        }
+
+        .github {
+            font-size: 1.2rem;
+            margin-left: 1rem;
+        }
     }
 }
 </style>
