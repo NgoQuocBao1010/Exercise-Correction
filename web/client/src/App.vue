1e825b296e5daa6d3d0c1a074542aba95c1fc2b5
ngoquocbao1010 <totti.chelsea@gmail.com>
2022-11-22 16:55:13 +0700

    👨‍💻 feat: fix footer css

--- web/client/src/App.vue
+++ web/client/src/App.vue
@@ -1,41 +1,49 @@
 <script setup></script>
 
 <template>
-    <!-- Navbar -->
-    <nav>
-        <RouterLink :to="{ name: 'Home' }" class="nav__header">
-            Exercise Correction
-        </RouterLink>
-    </nav>
+    <div class="site-container">
+        <!-- Navbar -->
+        <nav>
+            <RouterLink :to="{ name: 'Home' }" class="nav__header">
+                Exercise Correction
+            </RouterLink>
+        </nav>
 
-    <!-- Main Content -->
-    <main>
-        <!-- Page View -->
-        <RouterView />
-    </main>
+        <!-- Main Content -->
+        <main>
+            <!-- Page View -->
+            <RouterView />
+        </main>
 
-    <!-- Footer -->
-    <footer>
-        <a
-            href="https://twitter.com/ngohquocbao1010"
-            target="_blank"
-            class="twitter"
-        >
-            @NgoQuocBao
-        </a>
+        <!-- Footer -->
+        <footer>
+            <a
+                href="https://twitter.com/ngohquocbao1010"
+                target="_blank"
+                class="twitter"
+            >
+                @NgoQuocBao
+            </a>
 
-        <a
-            href="https://github.com/NgoQuocBao1010/Exercise-Correction"
-            target="_blank"
-            class="github"
-        >
-            <i class="fa-brands fa-github"></i>
-        </a>
-    </footer>
+            <a
+                href="https://github.com/NgoQuocBao1010/Exercise-Correction"
+                target="_blank"
+                class="github"
+            >
+                <i class="fa-brands fa-github"></i>
+            </a>
+        </footer>
+    </div>
 </template>
 
 <style lang="scss">
 body {
+    .site-container {
+        min-height: 100vh;
+        display: flex;
+        flex-direction: column;
+    }
+
     nav {
         margin: 3rem 0;
         text-align: center;
@@ -56,13 +64,10 @@ body {
     }
 
     footer {
+        margin-top: auto;
+        margin-inline: auto;
         width: 90vw;
         border-top: 1px solid var(--primary-color);
-        position: fixed;
-        bottom: 0;
-        left: 50%;
-        transform: translateX(-50%);
-        margin-inline: auto;
         padding: 0.3rem 0;
         text-align: center;
 
