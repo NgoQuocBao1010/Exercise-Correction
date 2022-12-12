fd271a0bf900d0253143180bf380f873b7a23dd0
ngoquocbao1010 <totti.chelsea@gmail.com>
2022-12-12 17:00:16 +0700

    👨‍💻 feat: Django match all unused routes for FE

--- web/server/exercise_correction/urls.py
+++ web/server/exercise_correction/urls.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Match all other routes for Frontend to handle
+urlpatterns += [
+    path("", TemplateView.as_view(template_name="index.html"), {"resource": ""}),
+    path("<path:resource>", TemplateView.as_view(template_name="index.html")),
+]
